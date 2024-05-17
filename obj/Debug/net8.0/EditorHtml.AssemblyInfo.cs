b5163d1fc916c7baea51142cace7b31239ac2ba9
Julio Cesar <jccardosodias0@gmail.com>
2024-05-17 08:06:10 -0300

    desenho do menu

--- obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
+++ obj/Debug/net8.0/EditorHtml.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+698a5c62b4a9c8d6194e4c39f8dbf477002a617f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EditorHtml")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
